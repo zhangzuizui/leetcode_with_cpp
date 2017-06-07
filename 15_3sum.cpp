b04248beec05005ab23103daec23efdf9caded7d
zhangzuizui <421350889@qq.com>
2017-06-07 15:03:40 +0800

    15

---
+++ 15_3sum.cpp
@@ -0,0 +1,45 @@
+class Solution {
+public:
+    vector<vector<int>> threeSum(vector<int>& nums) {
+        
+        vector<vector<int>> result;
+        int left, right, pre;
+        
+        if (nums.size() < 3)
+            return result;
+        
+        sort(nums.begin(), nums.end(), [](int a, int b){return a < b;});
+        
+        if (*(nums.end()-1) < 0) 
+            return result;
+            
+        for (int i = 0; i < nums.size()-2; ++i){
+            
+            if (nums[i] > 0) break;
+            if (i != 0 && nums[i] == nums[i-1]) continue;
+            
+            left = i+1, right = nums.size()-1, pre = nums[0]-1;
+            
+            while (left < right){
+                
+                if (nums[left]+nums[right] == -nums[i]){
+                    
+                    if (nums[left] == pre)
+                        ++left;
+                    else{
+                        result.push_back(vector<int> {nums[i], nums[left], nums[right]});
+                        pre = nums[left];
+                        ++left;
+                    }
+                }
+                
+                else if (nums[left]+nums[right] < -nums[i])
+                    ++left;
+                else 
+                    --right;
+                    
+            }
+        }
+        return result;
+    }
+};
