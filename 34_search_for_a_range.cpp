b15f60a1c211a3e196dd35ec8fbff0e8e5cc7712
zhangzuizui <421350889@qq.com>
2017-06-08 11:36:22 +0800

    34

---
+++ 34_search_for_a_range.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    vector<int> searchRange(vector<int>& nums, int target) {
+        
+        if (nums.empty())
+            return vector<int> {-1, -1};
+        if (target < *nums.begin() || target > *(nums.end()-1))
+            return vector<int> {-1, -1};
+            
+        int l_l = 0, l_r = nums.size()-1, l_m;
+        while (l_l < l_r){
+            l_m = (l_l + l_r) >> 1;
+            if (nums[l_m] < target)
+                l_l = l_m + 1;
+            else 
+                l_r = l_m;
+        }
+        if (nums[l_r] != target)
+            return vector<int> {-1, -1};
+        int r_l = l_r, r_r = nums.size()-1, r_m;
+        while (r_l < r_r){
+            r_m = ((r_l + r_r) >> 1) + 1;
+            if (nums[r_m] > nums[r_l])
+                r_r = r_m - 1;
+            else
+                r_l = r_m ;
+        }
+        return vector<int> {l_r, r_l};
+    }
+};
